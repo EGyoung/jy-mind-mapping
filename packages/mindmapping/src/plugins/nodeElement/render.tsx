0f29d83d67c991f639ef31b585ef6ebdead8a9a8
yijuyang <yijuyang@cvte.com>
2024-05-26 19:01:21 +0800

    feat: 修改节点样式

--- packages/mindmapping/src/plugins/nodeElement/render.tsx
+++ packages/mindmapping/src/plugins/nodeElement/render.tsx
@@ -10,7 +10,10 @@ const NodeElementRender = ({ nodeData, ctx }: { nodeData: Node, ctx: TMindMappin
             style={{
                 position: 'absolute', top: nodeData.position.y, left: nodeData.position.x, width: nodeData.width, height: nodeData.height,
                 color: nodeData.color, backgroundColor: nodeData.backgroundColor,
-                border: isSelected ? '1px solid red' : '1px solid transparent'
+                border: isSelected ? '1px solid red' : '1px solid transparent',
+                borderRadius: 5,
+                textAlign: 'center',
+                lineHeight: `${nodeData.height}px`
             }}>
             {nodeData.text}
         </div>
