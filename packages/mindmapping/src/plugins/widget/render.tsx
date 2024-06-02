3d5a7f6a6abc11c2a792183928342873f4615695
yijuyang <yijuyang@cvte.com>
2024-06-02 19:39:42 +0800

    feat: 添加widget配置及渲染

--- packages/mindmapping/src/plugins/widget/render.tsx
+++ packages/mindmapping/src/plugins/widget/render.tsx
@@ -1,19 +1,46 @@
 import { AddSvg } from "./addSvg"
 import React from 'react';
+const commonStyle = {
+    position: 'absolute',
+    border: 'none',
+    lineHeight: 0,
+    cursor: 'pointer',
+} satisfies React.CSSProperties;
+const divConfigs = {
+    top: {
+        top: 0,
+        left: '50%',
+        transform: 'translate(-50%, -100%)'
+    },
+    bottom: {
+        bottom: '0px',
+        left: '50%',
+        transform: 'translate(-50%, 100%) rotate(180deg)'
+    },
+    left: {
+        top: '50%',
+        transform: 'translate(-72%, -50%) rotate(-90deg)',
+        left: 0
+    },
+    right: {
+        top: '50%',
+        transform: 'translate(72%, -50%) rotate(90deg)',
+        right: 0
+    }
+} satisfies Record<string, React.CSSProperties>;
+
 
 
 const AddNodeWrapperRender = () => {
     return (
-        <div style={{
-            position: 'absolute',
-            border: 'none',
-            lineHeight: 0,
-            top: 0,
-            left: '50%',
-            transform: 'translate(-50%, -100%)'
-        }}>
-            {AddSvg}
-        </div>
+        <>
+            {Object.entries(divConfigs).map(([key, style]) => (
+                <div key={key} style={{ ...commonStyle, ...style } as any}>
+                    {AddSvg}
+                </div>
+            ))}
+        </>
+
     )
 }
 
