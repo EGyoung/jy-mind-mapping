a82aa549a36a062af09dc0bd0e5eba2dae172eed
yijuyang <yijuyang@cvte.com>
2024-06-22 15:46:47 +0800

    feat: 计算偏移量

--- packages/mindmapping/src/components/container/index.tsx
+++ packages/mindmapping/src/components/container/index.tsx
@@ -3,10 +3,17 @@ import type { TMindMappingCore } from '../../core'
 import './index.less'
 const Container = ({ children, mindMap }: { children: any, mindMap: TMindMappingCore }) => {
     const info = useRef(mindMap.Layout.getContainerLayout())
+    const getPosition = () => {
+        const height = info.current.height
+        const rootHeight = mindMap.getConfig()?.height ?? 0
+        return {
+            top: (height - rootHeight) / 2
+        }
+    }
     return (
         <div className='jy-mind-mapping-container' data-id={mindMap.id}>
             <div className='jy-mind-mapping-entity' style={{ width: info.current.width, height: info.current.height }}>
-                <div style={{ transform: 'translate(0, 50px)' }}>
+                <div style={{ position: 'relative', ...getPosition() }}>
                     {children}
                 </div>
             </div>
